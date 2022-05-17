4323b51d40d08abbbbb1666b211ed4b1882ff3ae
khetu <50918269+khetulal@users.noreply.github.com>
2022-05-17 17:58:35 +0545

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,43 +1,21 @@
 import 'package:flutter/material.dart';
+import 'SignUp.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "profile App",
-    home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('profile App'),
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sign Up',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          Center(
-              child: Image.network(
-            "https://media-exp1.licdn.com/dms/image/C4E03AQEASEq60zmbng/profile-displayphoto-shrink_200_200/0/1618218377714?e=1655337600&v=beta&t=MIghp4Y6Xy0JOwIIbgLvBBImRN706RfSDl5ZC5G7sP0",
-            height: 300,
-            width: 300,
-          )),
-          SizedBox(height: 10),
-          Text(
-            "khetulal kc",
-            style: TextStyle(
-                fontSize: 24, color: Colors.green, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            'Address: Dang, Nepal',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text('Email:khetulalkhatri15852@gmail.com'),
-          Text('Develop By: khetulal')
-        ],
-      ),
-    ),
-  ));
+      home: SignUp(),
+    );
+  }
 }
